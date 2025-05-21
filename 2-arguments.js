77cd2015ac29087878428e6f202af49d1bfdd381
Meshack Otieno <meshackb59@gmail.com>
2025-05-21 14:41:13 +0300

    my argument file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
-// Get the number of arguments passed (excluding the first two default args)
-const args = process.argv;
+// Use const to declare the variable holding the arguments
+const args = process.argv.slice(2);
 
-// Determine and print the appropriate message
+// Check the number of arguments and print the correct message
 if (args.length === 0) {
   console.log("No argument");
 } else if (args.length === 1) {
