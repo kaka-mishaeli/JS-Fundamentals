64b6f81fe3ee2f57d80cd6b96079334b70aa503b
Meshack Otieno <meshackb59@gmail.com>
2025-05-21 15:46:23 +0300

    my file missing

--- example.js
+++ example.js
@@ -1,1 +1,10 @@
-console.log("the process is :",  Process.pid);
+const num = 5
+while (num <5){
+    if (num != NaN){
+        console.log('missing number of occurrences')
+    }
+    else {
+    console.log('C is fun')
+    }
+    num++;
+};
