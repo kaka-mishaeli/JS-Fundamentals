c41939e9f2c7930c17ebc0bf780ee858332dfdea
Meshack Otieno <meshackb59@gmail.com>
2025-05-21 15:27:31 +0300

    my added statements

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,9 @@
-const lang = ['C is fun', 'Python is cool','Javascript is amazing'];
+/*const lang = ['C is fun', 'Python is cool','Javascript is amazing'];
 for (let i = 0; i < lang.length; i++){
     console.log(lang[i]);
-} 
+} */
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
